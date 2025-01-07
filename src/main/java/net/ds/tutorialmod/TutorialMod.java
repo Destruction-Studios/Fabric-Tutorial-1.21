6c4740c0e94d48cdf5f0674bf66d629698901747
Destruction-Studios <founderofbeans@gmail.com>
2025-01-07 12:54:39 -0500

    Added Items

--- src/main/java/net/ds/tutorialmod/TutorialMod.java
+++ src/main/java/net/ds/tutorialmod/TutorialMod.java
@@ -1,18 +1,19 @@
 package net.ds.tutorialmod;
 
+import net.ds.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
